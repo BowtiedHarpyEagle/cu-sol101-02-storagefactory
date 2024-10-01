e7979c5408794d274e61789e96d7dc3424e4751f
BowtiedHarpyEagle <bowtiedharpyeagle@proton.me>
2024-10-01 09:38:25 -0500

    add simplestorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract SimpleStorage {
+    uint256 myFavoriteNumber; // initializes to 0 by default, visibility defaults to internal
+    
+    //Struct defines a new data type Person with a favoriteNumber and a name
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+
+    mapping (string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push( Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return myFavoriteNumber;
+    }
+}
