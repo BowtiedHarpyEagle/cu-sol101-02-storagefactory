232b1c86ac3393371997c94823488724e24eabbe
BowtiedHarpyEagle <bowtiedharpyeagle@proton.me>
2024-10-01 10:02:42 -0500

    complete l4 solidity imports

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity ^0.8.18;
 
 contract SimpleStorage {
     uint256 myFavoriteNumber; // initializes to 0 by default, visibility defaults to internal
